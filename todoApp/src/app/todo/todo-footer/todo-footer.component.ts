f3b93e8210f08bc168e3630c55498850a6595aeb
Aurelio <aurelio@sparanzza.com>
2020-02-15 23:56:04 +0100

    Add style filter in footer

--- todoApp/src/app/todo/todo-footer/todo-footer.component.ts
+++ todoApp/src/app/todo/todo-footer/todo-footer.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AppState } from "src/app/app.reducers";
+import { Store } from "@ngrx/store";
+import { Component, OnInit } from "@angular/core";
+import * as fromFilter from "../../filter/filter.actions";
 @Component({
-  selector: 'app-todo-footer',
-  templateUrl: './todo-footer.component.html',
+  selector: "app-todo-footer",
+  templateUrl: "./todo-footer.component.html",
   styles: []
 })
 export class TodoFooterComponent implements OnInit {
-
-  constructor() { }
+  validFilters: fromFilter.validFilters[] = ["all", "completed", "pending"];
+  currentFilter: fromFilter.validFilters;
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
+    this.store.subscribe(state => {
+      this.currentFilter = state.filter;
+    });
   }
 
+  changeFilter(newFilter: fromFilter.validFilters) {
+    const action = new fromFilter.SetFilterAction(newFilter);
+    this.store.dispatch(action);
+  }
 }
