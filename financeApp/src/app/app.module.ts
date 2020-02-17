179fe6477681afb05da6485862f84724dfd5c480
Aurelio <aurelio@sparanzza.com>
2020-02-17 21:30:52 +0100

    init financeApp example and  add several components

---
+++ financeApp/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { StadisticComponent } from './deposit/stadistic/stadistic.component';
+import { DetailComponent } from './deposit/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    DepositComponent,
+    StadisticComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ financeApp/src/app/deposit/deposit.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-deposit",
+  templateUrl: "./deposit.component.html",
+  styleUrls: []
+})
+export class DepositComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
